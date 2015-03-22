5be79d0d76c6849f38715032d3dbda010237540d
Yan Xu <xuyantc@msn.com>
2015-03-21 23:00:35 -0400

    try to resolve the type dynamic

--- com.pxz.log8430.menutp3/src/com/pxz/log8430/menutp3/DefineMenuTp3.java
+++ com.pxz.log8430.menutp3/src/com/pxz/log8430/menutp3/DefineMenuTp3.java
@@ -4,72 +4,108 @@ import org.eclipse.core.runtime.IConfigurationElement;
 import org.eclipse.core.runtime.IExtension;
 import org.eclipse.core.runtime.IExtensionPoint;
 import org.eclipse.core.runtime.Platform;
-import org.eclipse.jface.action.IMenuListener;
-import org.eclipse.jface.action.IMenuManager;
 import org.eclipse.jface.action.MenuManager;
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.widgets.MenuItem;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.menus.CommandContributionItem;
 import org.eclipse.ui.menus.CommandContributionItemParameter;
 import org.eclipse.ui.menus.ExtensionContributionFactory;
 import org.eclipse.ui.menus.IContributionRoot;
 import org.eclipse.ui.services.IServiceLocator;
+import org.eclipse.jdt.core.ICompilationUnit;
 
 public class DefineMenuTp3 extends ExtensionContributionFactory {
+	
+	final private String EXTENTION_ID = "com.pxz.menutp3";
+	final private String CONTTRIBUTE_COMMAND_LABEL = "commandLabel";
+	final private String CONTTRIBUTE_COMMAND_TARGET_TYPE = "commandTargetType";
+	final private String CONTTRIBUTE_COMMAND_ID = "commandId";
 
 	@Override
 	public void createContributionItems(IServiceLocator serviceLocator,
 			IContributionRoot additions) {
 		
 		System.out.println("start to create contribution items");
-		final String EXTENTION_ID = "com.pxz.menutp3";
-		final String CONTTRIBUTE_NAME = "name";
-		final String CONTTRIBUTE_FOLDER = "for_folder";
-		final String CONTTRIBUTE_FILE = "for_file";
-		final String CONTTRIBUTE_COMMAND_ID = "commandId";
-		
-		
-		//IExtension e = Platform.getExtensionRegistry().getExtension(EXTENTION_ID);
-		
+				
 		IExtensionPoint ep = Platform.getExtensionRegistry().getExtensionPoint(EXTENTION_ID);
 		
-		IExtension[] extensions = ep.getExtensions(); //get all extensions
+		IExtension[] extensions = ep.getExtensions(); //get all extensions that extend this extension point
 		
-		MenuManager menu = new MenuManager("Menu Tp3");
+		MenuManager menu = new MenuManager("Command Menu"); //create a menu item to contain commands in sub-extensions
 		
+		// get the selected element
+		IWorkbenchWindow window = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+		IStructuredSelection selection = (IStructuredSelection) window.getSelectionService().getSelection();
+        Object selectedElement = selection.getFirstElement();
+         
+
 		
 		for(int i = 0; i < extensions.length; i++) {
-			String menuName = null;
-			String menuForFoleder = null;
-			String menuForFile = null;
 			
+			String menuLabel = null;
 			String commandID = null;
+			String commandTargetType = null;
 			
 			IConfigurationElement[] configurations = extensions[i].getConfigurationElements();
 			
 			for(int j = 0; j < configurations.length; j++) {
-				if("menu".equals(configurations[j].getName())) {
-					menuName = configurations[j].getAttribute(CONTTRIBUTE_NAME);
-					menuForFoleder = configurations[j].getAttribute(CONTTRIBUTE_FOLDER);
-					menuForFile = configurations[j].getAttribute(CONTTRIBUTE_FILE);
+				if("commandDescription".equals(configurations[j].getName())) {
+					menuLabel = configurations[j].getAttribute(CONTTRIBUTE_COMMAND_LABEL);
+					commandTargetType = configurations[j].getAttribute(CONTTRIBUTE_COMMAND_TARGET_TYPE);
 					commandID = configurations[j].getAttribute(CONTTRIBUTE_COMMAND_ID);
-					System.out.println("commandID " + commandID);
+										
+					break; //it can only contain one command
+				}
+			}
+			
+			
+			if (menuLabel != null && commandID != null && commandTargetType != null){
+			
+				// To judge if the selected element's type correspond to the specified target type
+				boolean selectedElementTypeCorrespondCommandTargetType = false;
+				
+				String[] types = commandTargetType.split(",");
+				
+				for (int k=0; k < types.length; k++){
+					if(selectedElement instanceof ICompilationUnit) {
+						selectedElementTypeCorrespondCommandTargetType = true;
+						break;
+					} /*
+					try {
+						//Class<?> aClass = Class.forName("org.eclipse.jdt.core.ICompilationUnit");//types[k]);
+						
+						//if (aClass.isInstance(selectedElement)){
+						if(selectedElement instanceof ICompilationUnit) {
+							selectedElementTypeCorrespondCommandTargetType = true;
+							break;
+						}
+					} catch(ClassNotFoundException ex) {
+				        System.out.println(ex.toString());
+				    }*/
 				}
 				
+				
+				if (selectedElementTypeCorrespondCommandTargetType){
+					
+					// add the sub-extension's command to the popup menu
+					CommandContributionItemParameter p = 
+		                    new CommandContributionItemParameter(
+		                            serviceLocator, 
+		                            extensions[i].getUniqueIdentifier(), 
+		                            commandID,
+		                            CommandContributionItem.STYLE_PUSH);
+		
+		            p.label = menuLabel;      
+		            menu.add(new CommandContributionItem(p));
+				}
 			}
-			//"org.eclipse.ui.file.exit"
-			CommandContributionItemParameter p = 
-                    new CommandContributionItemParameter(
-                            serviceLocator, 
-                            extensions[i].getUniqueIdentifier(), 
-                            commandID,
-                            CommandContributionItem.STYLE_PUSH);
-
-            p.label = menuName;      
-            menu.add(new CommandContributionItem(p));
 		}
 		
-		additions.addContributionItem(menu, null);
+		// if there is no correspond command, don't add the menu to the root
+		if (menu.getSize() > 0){
+			additions.addContributionItem(menu, null);
+		}
 		
 	}
 
