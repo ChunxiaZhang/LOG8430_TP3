6c02bd02af271271e55eb618401b4a1ad59ba618
oPinon <olivier.pinon.mail@gmail.com>
2015-03-23 00:33:12 -0400

    Made the "showPath" command more general (works with IResource and IJavaElement)

--- com.pxz.log8430.path/src/com/pxz/log8430/path/ShowPath.java
+++ com.pxz.log8430.path/src/com/pxz/log8430/path/ShowPath.java
@@ -3,10 +3,9 @@ package com.pxz.log8430.path;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.IPath;
-import org.eclipse.jdt.core.ICompilationUnit;
-import org.eclipse.jdt.core.IPackageFragment;
-import org.eclipse.jdt.core.IPackageFragmentRoot;
+import org.eclipse.jdt.core.IJavaElement;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.handlers.HandlerUtil;
@@ -18,25 +17,26 @@ public class ShowPath extends AbstractHandler {
 		
 		IPath path = null;
 		
+		// get the selected element
 		IStructuredSelection selection = (IStructuredSelection) HandlerUtil
 		        .getActiveSite(event).getSelectionProvider().getSelection();
 		    if (selection == null) {
 		      return null;
 		    }
 		Object firstElement = selection.getFirstElement();
-		if(firstElement instanceof ICompilationUnit) {
-        	ICompilationUnit element = (ICompilationUnit)firstElement;
-        	path = element.getResource().getLocation();
-
-        } else if(firstElement instanceof IPackageFragmentRoot) {
-        	IPackageFragmentRoot element = (IPackageFragmentRoot) firstElement; 
-        	path = element.getResource().getLocation();
-        	
-        } else if(firstElement instanceof IPackageFragment) {
-        	IPackageFragment element = (IPackageFragment) firstElement; 
-        	path = element.getResource().getLocation();
-        } 
 		
+		// get the element's type
+		if(firstElement instanceof IResource) {
+        	IResource element = (IResource)firstElement;
+        	path = element.getLocation();
+
+        } else if(firstElement instanceof IJavaElement) {
+        	IJavaElement element = (IJavaElement) firstElement;
+        	path = element.getPath();
+        	
+        }
+		
+		// print the element's path
 		if(path != null) {
 			
 			MessageDialog.openInformation(HandlerUtil.getActiveWorkbenchWindow(event).getShell(),
